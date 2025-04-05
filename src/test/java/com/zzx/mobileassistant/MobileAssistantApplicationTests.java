8ead17b9e2a474489ebb52e936b8631c839fc190
ivamly <ivmaly1@yandex.ru>
2025-04-05 13:04:02 +0300

    init
    Signed-off-by: ivamly <ivmaly1@yandex.ru>

---
+++ src/test/java/com/zzx/mobileassistant/MobileAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.mobileassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
